039e8830b7bb4c590615ae2db33acbce67d6a879
Maven <48680434+MikeMutter@users.noreply.github.com>
2020-01-09 10:08:29 -0600

    Update Maven_Debian_setup.sh

--- Maven_Debian_setup.sh
+++ Maven_Debian_setup.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+logo(){
+  #against the law to have a batch script without a cool logo :D
+  echo "${red}
+      ||\\  //||   //\\  \\    // ||''' ||\\  || 
+      || \\// ||  //==\\  \\  //  ||==  || \\ ||
+      ||      || //    \\  \\//   ||,,, ||  \\||             
+${reset}                                     "
+}
 ########################################################################
-#      |\  /|   /\  \    / |`` |\  | 
-#      | \/ |  /~~\  \  /  |-- | \ |
-#      |    | /    \  \/   |~~ |  \|
 # Date: 01/09/2020
 # Combined several scripts to update outdated apps and add some custom
 # firmaware and dependancies for my laptop
 ########################################################################
+
+logo
 
 export DEBIAN_FRONTEND=noninteractive;
 echo "[****] Maven Initial Setup [****]"
@@ -25,19 +32,35 @@ apt-get -y install wireless-tools
 apt-get -y install network-manager-gnome
 apt-get -y rar
 apt-get -y install transmission-cli
-apt-get install -y build-essential python3 python3-dev wget unzip chromium-browser gcc make libpcap-dev python3-pip 
-ln -s /usr/bin/python3 /usr/bin/python
-pip3 install jsbeautifier
-apt install sudo -y
+apt-get install -y libcurl4-openssl-dev
+apt-get install -y libssl-dev
+apt-get install -y jq
+apt-get install -y ruby-full
+apt-get install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
+apt-get install -y build-essential libssl-dev libffi-dev python-dev
+apt-get install -y python-setuptools
+apt-get install -y libldns-dev
+apt-get install -y python3-pip
+apt-get install -y python-pip
+apt-get install -y python-dnspython
+apt-get install -y git
+apt-get install -y rename
+apt-get install -y xargs
+pip3 install -y jsbeautifier
+
 
 echo "[*] Install GoLang [*]"
 wget https://storage.googleapis.com/golang/go1.13.5.linux-amd64.tar.gz
 tar -xvg go1.13.5.linux-amd64.tar.gz
 mv go/ /usr/local
-echo "export GOROOT=/usr/local/go" >> $HOME/.bash_profile 
-echo "export GOPATH=$HOME/mike/go" >> $HOME/.bash_profile 
-echo "export PATH=$GOROOT/bin:$GOPATH/bin:$PATH" >> $HOME/.bash_profile && source $HOME/.bash_profile
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+echo "export GOROOT=/usr/local/go" >> ~/.bash_profile 
+echo "export GOPATH=$HOME/go" >> ~/.bash_profile 
+echo "export PATH=$GOROOT/bin:$GOPATH/bin:$PATH" >> ~/.bash_profile && source ~/.bash_profile
 
+apt install sudo -y
 ####Custom to me or my Laptop########
 usermod -aG sudo  mike
 apt-get -y install linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,')
@@ -88,6 +111,18 @@ dpkg -i --force-depends ~/google-chrome-stable_current_amd64.deb
 
 echo " [-------------------] Bug Bounty Programs & Scipts [-------------------]"
 
+#create a tools folder in ~/
+mkdir ~/tools
+cd ~/tools/
+
+echo "installing bash_profile aliases from recon_profile"
+git clone https://github.com/nahamsec/recon_profile.git
+cd recon_profile
+cat bash_profile >> ~/.bash_profile
+source ~/.bash_profile
+cd ~/tools/
+echo "done"
+
 echo "[*] Install Lazy Recon Script [*]"
 curl -o lazyrecon.sh https://raw.githubusercontent.com/nahamsec/lazyrecon/master/lazyrecon.sh
 dos2unix lazyrecon.sh
@@ -105,6 +140,15 @@ export GO111MODULE=on
 go get -v -u github.com/OWASP/Amass/v3/...
 cp $HOME/go/bin/amass /usr/local/bin
 
+echo "[+] Installing sublist3r [+]"
+git clone https://github.com/aboul3la/Sublist3r.git $HOME/tools/sublist3r
+pip3 install --no-cache-dir --install-option="--prefix=/install" -r $HOME/tools/sublist3r/requirements.txt
+PATH_EXPORT=$PATH:$HOME/tools/sublist3r
+echo $PATH_EXPORT
+echo "export PATH=${PATH_EXPORT}" >> $HOME/.bash_profile && source $HOME/.bash_profile
+ln -sf $HOME/tools/sublist3r/sublist3r.py $HOME/tools/sublist3r/sublist3r && chmod +x $HOME/tools/sublist3r/sublist3r
+echo "[+] Finished installing sublist3r [+]"
+    
 echo "[*] Installing Subjack [*] "
 go get github.com/haccer/subjack
 cp $HOME/go/bin/subjack /usr/local/bin
@@ -143,6 +187,10 @@ echo "[+] Finished installing Meg [+]"
 
 echo "[+] Installing Dirsearch [+] "
 git clone https://github.com/maurosoria/dirsearch.git
+PATH_EXPORT=$PATH:$HOME/tools/dirsearch
+echo $PATH_EXPORT
+echo "export PATH=${PATH_EXPORT}" >> $HOME/.bash_profile && source $HOME/.bash_profile
+ln -sf $HOME/tools/dirsearch/dirsearch.py $HOME/tools/dirsearch/dirsearch && chmod +x $HOME/tools/dirsearch/dirsearch
 echo "[+] Finished installing Dirsearch [+] "
 
 echo "[*] Installing aquatone [*]"
@@ -154,6 +202,10 @@ echo "[+] Finished installing aquatone [+]"
 
 echo "[+] Installing LinkFinder [+] "
 git clone https://github.com/GerbenJavado/LinkFinder.git
+PATH_EXPORT=$PATH:$HOME/tools/LinkFinder
+echo $PATH_EXPORT
+echo "export PATH=${PATH_EXPORT}" >> $HOME/.bash_profile && source $HOME/.bash_profile
+ln -sf $HOME/tools/LinkFinder/linkfinder.py $HOME/tools/LinkFinder/linkfinder && chmod +x $HOME/tools/LinkFinder/linkfinder
 cd LinkFinder
 python setup.py install
 cd $HOME
@@ -163,4 +215,9 @@ echo "[+] Installing inscope [+] "
 go get -u github.com/tomnomnom/hacks/inscope
 echo "[+] Finished installing inscope [+]" 
  
+echo "[+] Installing MassDNS [+]"
+git clone https://github.com/blechschmidt/massdns.git $HOME/tools/massdns
+cd $HOME/tools/massdns && make && cp $HOME/tools/massdns/bin/massdns /usr/local/bin
+echo "[+] Finished installing MassDNS [+]"
+    
 echo "[*]Finished Installing All Apps[*]"
