c56cfc178abafd9506f119d3edad73f273ae907b
Maven <48680434+MikeMutter@users.noreply.github.com>
2021-02-11 11:53:20 -0600

    Add files via upload
    latest script to download my commonly used programs. ***Before test****

---
+++ NewDebianMaven.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+logo(){
+  #against the law to have a batch script without a cool logo :D
+  echo "${red}
+      ||\\  //||   //\\  \\    // ||''' ||\\  || 
+      || \\// ||  //==\\  \\  //  ||==  || \\ ||
+      ||      || //    \\  \\//   ||,,, ||  \\||             
+${reset}                                     "
+}
+########################################################################
+# Date: 02/10/2021
+# Combined several scripts to update outdated apps and add some custom
+# firmaware and dependancies for my laptop
+########################################################################
+
+logo
+
+export DEBIAN_FRONTEND=noninteractive;
+echo "[****] Maven Initial Setup [****]"
+echo "[****] Upgrade Installed Packages to Latest [****]"
+
+#####Cut here if you dont want to autoclean##########
+apt-get -qq update && apt-get -qq dist-upgrade -y
+apt full-upgrade -y
+apt-get autoclean
+#####################################################
+
+echo -e "\nInstalling default packages and dependancies...\n"
+apt-get -y install aptitude
+apt-get -y install wireless-tools
+apt-get -y install network-manager-gnome
+apt-get -y install transmission-cli
+apt-get install -y libcurl4-openssl-dev
+apt-get install -y libssl-dev
+apt-get install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
+apt-get install -y build-essential libssl-dev libffi-dev python-dev
+apt-get install -y python-setuptools
+apt-get install -y libldns-dev
+apt-get install -y python3-pip
+apt-get install -y python-pip
+apt-get install -y python-dnspython
+apt-get install -y rename
+apt-get install -y xargs
+apt-get install -y git
+apt-get -y rar
+##Antivirus#################
+echo "[*] Install ClamAV [*]"
+apt-get install clamav -y 
+echo "[*] Finished installing ClamAV [*]"
+#############################
+
+
+apt-get install sudo -y
+
+####Custom to me or my Laptop########
+usermod -aG sudo  maven
+apt-get -y install linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,')
+apt-get -y install firmware-linux-nonfree
+apt install firmware-linux-nonfree intel-microcode -y
+apt install firmware-linux firmware-linux-nonfree libdrm-amdgpu1 xserver-xorg-video-amdgpu -y
+apt-get -y install gdebi gksu firmware-linux
+apt-get -y install broadcom-sta-dkms 
+apt-get -y install firmware-iwlwifi
+modprobe -r iwlwifi ; modprobe iwlwifi
+
+echo "[++++] Install IP Vanish [++++]"
+cd Downloads/
+wget https://www.ipvanish.com/software/configs/configs.zip
+unzip configs.zip
+cd ..
+echo "[+++] go set up the VPN network manager and disable IPV6 [++++] "
+
+echo "[+++] Set up firewall [++++]"
+apt-get -y install ufw
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow ssh
+ufw enable
+
+#####################################
+
+echo " [-------------------] Common Programs[-------------------]"
+
+echo "[*] keepnote [*]"
+apt-get -y install keepnote
+
+echo "[+++] install FlameShot [++++]"
+apt install flameshot -y
+
+###############Cut for Kali machine as well##############
+
+##################Intall Go and set enviroment##############################################
+echo "[*] Install GoLang [*]"
+wget https://storage.googleapis.com/golang/go1.13.5.linux-amd64.tar.gz
+tar -xvg go1.13.5.linux-amd64.tar.gz
+mv go/ /usr/local
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+echo "export GOROOT=/usr/local/go" >> ~/.bash_profile 
+echo "export GOPATH=$HOME/go" >> ~/.bash_profile 
+echo "export PATH=$GOROOT/bin:$GOPATH/bin:$PATH" >> ~/.bash_profile && source ~/.bash_profile
+##############################################################################################
+echo "[*] Gedit [*]"
+apt-get -y install gedit
+
+echo "[*] Gedit [*]"
+apt-get -y install dnsutils
+
+echo "[*] tmux [*]"
+apt-get -y install tmux
+
+echo "[*] visualcode [*]"
+apt install software-properties-common apt-transport-https curl
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+apt update
+apt install code
+#############################################################
+echo "[====] Install Burp Suite [====]"
+cd ~
+wget https://portswigger.net/burp/releases/download?product=community&version=2.1.07&type=linux&componentid=100
+
+echo "[*] Installing Nmap [*] "
+apt-get install -y nmap
+echo "[*] Finished installing Nmap [*] "
+
+echo "[*] Installing WireShark [*] "
+apt-get install -y wireshark
+echo "[*] Finished installing WireShark [*] "
+
+echo "[+] Installing and starting OWASP ZAP [+]"
+wget https://github.com/zaproxy/zaproxy/releases/download/w2020-01-27/ZAP_WEEKLY_D-2020-01-27.zip
+unzip ZAP_WEEKLY_D-2020-01-27.zip
+echo "[+] Finished installing OWASP ZAP [+]"
+
+echo "[++++] Install VMWare [++++]"
+mkdir VM
+cd VM/
+wget https://www.vmware.com/go/getplayer-linux
+echo "[+]Install back up kali VM [+]"
+cd Kali-Linux-2020.4-vmware-amd64.vmwarevm/
+wget https://github.com/MikeMutter/Backup_VM/raw/main/VM_BackUp_OSCP.zip
+unzip VM_BackUp_OSCP.zip
+cd VM_BackUp_OSCP
+cp Kali-Linux-2020* /home/maven/VM/Kali-Linux-2020.4-vmware-amd64.vmwarevm
+echo "[+++++++] "
+
+
+    
+echo "[*]Finished Installing All Apps[*]"
